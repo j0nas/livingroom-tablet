9bd111d0375b45cc9b5e5e7b11969eb9b40ab9b5
Jonas Jensen <jonasj@mesan.no>
2016-08-22 22:19:41 +0200

    recieve messages

--- src/App/index.js
+++ src/App/index.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			messages: []
+		}
+	}
+
 	pushToFirebase() {
 		const baseInput = document.getElementById('baseinput');
 		firebase.database().ref('message').push({message: baseInput.value});
-		baseinput.value = "";
+		baseInput.value = "";
 	}
 
 	componentDidMount() {
@@ -18,11 +25,27 @@ class App extends Component {
 			storageBucket: "livingroom-tablet.appspot.com",
 		};
 		firebase.initializeApp(config);
+
+		const msgRef = firebase.database().ref('message');
+		const thisRef = this;
+
+		msgRef.on('value', function(snapshot) {
+			const messageKeys = Object.keys(snapshot.val());
+			const messageStrings = messageKeys.map(key => snapshot.val()[key].message);
+			console.log(messageStrings);
+
+			thisRef.setState({
+				messages: messageStrings
+			});
+		});
 	}
 
 	render() {
 		return (
 			<div className="App">
+			<div id="messagesDiv">
+				{this.state.messages.map(msg => <div key={msg + Math.random()}>{msg}</div>)}
+			</div>
 			<input type="text" id="baseinput"></input>
 
 			<button onClick={this.pushToFirebase}>PUSH</button>
