48e2bd80f1876c25bb5eb0f64f973c23be9a2fe2
Jonas Jensen <jonasj@mesan.no>
2016-08-24 18:14:59 +0200

    reformat App index

--- src/App/index.js
+++ src/App/index.js
@@ -1,56 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import firebase from 'firebase';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			messages: []
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            messages: []
+        }
+    }
 
-	pushToFirebase() {
-		const baseInput = document.getElementById('baseinput');
-		firebase.database().ref('message').push({message: baseInput.value});
-		baseInput.value = "";
-	}
+    pushToFirebase() {
+        const baseInput = document.getElementById('baseinput');
+        firebase.database().ref('message').push({message: baseInput.value});
+        baseInput.value = "";
+    }
 
-	componentDidMount() {
-		const firebase = require('firebase');
-		const config = {
-			apiKey: "AIzaSyBeXvLz0iuYBk2Nm3pgV2XzADh6w6ioUlE",
-			authDomain: "livingroom-tablet.firebaseapp.com",
-			databaseURL: "https://livingroom-tablet.firebaseio.com",
-			storageBucket: "livingroom-tablet.appspot.com",
-		};
-		firebase.initializeApp(config);
+    componentDidMount() {
+        const firebase = require('firebase');
+        const config = {
+            apiKey: "AIzaSyBeXvLz0iuYBk2Nm3pgV2XzADh6w6ioUlE",
+            authDomain: "livingroom-tablet.firebaseapp.com",
+            databaseURL: "https://livingroom-tablet.firebaseio.com",
+            storageBucket: "livingroom-tablet.appspot.com",
+        };
+        firebase.initializeApp(config);
 
-		const msgRef = firebase.database().ref('message');
-		const thisRef = this;
+        const msgRef = firebase.database().ref('message');
+        const thisRef = this;
 
-		msgRef.on('value', function(snapshot) {
-			const messageKeys = Object.keys(snapshot.val());
-			const messageStrings = messageKeys.map(key => snapshot.val()[key].message);
+        msgRef.on('value', function (snapshot) {
+            const messageKeys = Object.keys(snapshot.val());
+            const messageStrings = messageKeys.map(key => snapshot.val()[key].message);
 
-			thisRef.setState({
-				messages: messageStrings
-			});
-		});
-	}
+            thisRef.setState({
+                messages: messageStrings
+            });
+        });
+    }
 
-	render() {
-		return (
-			<div className="App">
-			<div id="messagesDiv">
-				{this.state.messages.map(msg => <div key={msg + Math.random()}>{msg}</div>)}
-			</div>
-			<input type="text" id="baseinput"></input>
+    render() {
+        return (
+            <div className="App">
+                <div id="messagesDiv">
+                    {this.state.messages.map(msg => <div key={msg + Math.random()}>{msg}</div>)}
+                </div>
+                <input type="text" id="baseinput"/>
 
-			<button onClick={this.pushToFirebase}>PUSH</button>
-			</div>
-			);
-	}
+                <button onClick={this.pushToFirebase}>PUSH</button>
+            </div>
+        );
+    }
 }
 
 export default App;
