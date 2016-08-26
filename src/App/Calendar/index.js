0d43a3ba3f32681dd016772bd8cba76f35b77a55
Jonas Jensen <jonasj@mesan.no>
2016-08-26 16:05:25 +0200

    change calendar to refresh only itself every half hour

--- src/App/Calendar/index.js
+++ src/App/Calendar/index.js
@@ -4,27 +4,32 @@ import "./style.css";
 class Calendar extends Component {
     constructor() {
         super();
-        this.state = {
-            display: "block"
-        };
 
         this.refresh = this.refresh.bind(this);
-//        setTimeout(this.refresh, 3000);
+        this.refreshComponentEvery = 1000 * 60 * 30;
+        setTimeout(this.refresh, this.refreshComponentEvery);
     }
 
     refresh() {
-        const display = this.state.display === "none" ? "block" : "none";
-        this.setState({display: display});
-        setTimeout(this.refresh, 3000);
+        const calendarDiv = document.getElementById('calendarContainer');
+        const iframe = calendarDiv.childNodes[0];
+
+        calendarDiv.removeChild(iframe);
+        this.forceUpdate();
+        calendarDiv.appendChild(iframe);
+        this.forceUpdate();
+        setTimeout(this.refresh, this.refreshComponentEvery);
     }
 
 
     render() {
-        return <div  style={{display: this.state.display}}>
-            <iframe id="calendar"
-                    src="https://calendar.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=supershadypeople%40gmail.com&amp;color=%231B887A&amp;ctz=Europe%2FOslo"
-                    frameBorder="0" scrolling="no" />
-        </div>;
+        return (
+            <div id="calendarContainer">
+                <iframe id="calendar"
+                        src="https://calendar.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=supershadypeople%40gmail.com&amp;color=%231B887A&amp;ctz=Europe%2FOslo"
+                        frameBorder="0" scrolling="no"/>
+            </div>
+        );
     }
 }
 
