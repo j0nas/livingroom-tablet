2502e35bb07fa00ad247558ec68323575af3f990
Jonas Jensen <jonasj@mesan.no>
2016-09-01 18:56:05 +0200

    single-char fix

--- src/App/XkcdStrip/index.js
+++ src/App/XkcdStrip/index.js
@@ -13,7 +13,7 @@ class XkcdComponent extends Component {
     componentDidMount() {
         const $ = require('jquery');
         $.ajax({
-            url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
+            url: "https://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
             dataType: "json",
             jsonpCallback: "xkcddata",
             success: data =>
