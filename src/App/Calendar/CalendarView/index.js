cd33c09207f7c80d0c01344219cef680483ee7d3
Jonas Jensen <jonasj@mesan.no>
2016-09-04 12:38:04 +0200

    columnized date & time separately, add format for tomorrow

--- src/App/Calendar/CalendarView/index.js
+++ src/App/Calendar/CalendarView/index.js
@@ -8,14 +8,30 @@ const CalendarView = ({events}) => (
         <tbody id="calendarContain">
         {events.map(event => {
             const date = new Date(event.start.dateTime || event.start.date);
-            const isItToday = new Date().toDateString() === date.toDateString();
-
+            const today = new Date();
             const time = event.start.dateTime ? ' ' + fixPrefix(date.getHours()) + ':' + fixPrefix(date.getMinutes()) : '';
-            const dateString = (isItToday ? "Today" : fixPrefix(date.getDate()) + '.' + fixPrefix(date.getMonth() + 1)) + time;
+
+            const eventOccursThisMonthAndYear = (today.getMonth() + today.getFullYear()) - (date.getMonth() + date.getFullYear()) === 0;
+            const todayOrTomorrow = date.getDate() - today.getDate();
+            const isItToday = eventOccursThisMonthAndYear && todayOrTomorrow === 0;
+
+            let dateString = fixPrefix(date.getDate()) + '.' + fixPrefix(date.getMonth() + 1);
+            if (eventOccursThisMonthAndYear)
+                switch (todayOrTomorrow) {
+                    case 0:
+                        dateString = "Today";
+                        break;
+                    case 1:
+                        dateString = "Tomorrow";
+                        break;
+                    default:
+                        break;
+                }
 
             return (
                 <tr key={shortid.generate()} className={isItToday ? "calendarEventToday" : ""}>
                     <td className="calendarDate">{dateString}</td>
+                    <td className="calendarTime">{time}</td>
                     <td className="calendarSummary">{event.summary}</td>
                 </tr>
             );
