338f3f804b81c88ea5095c91670aab50cb8e7f99
Jonas Jensen <jonasj@mesan.no>
2016-09-04 11:55:56 +0200

    style calendar to display neat table

--- src/App/Calendar/CalendarView/index.js
+++ src/App/Calendar/CalendarView/index.js
@@ -4,16 +4,21 @@ import {prefixWithZeroIfSingleDigit as fixPrefix} from "../../util/CalendarForma
 import "./style.css";
 
 const CalendarView = ({events}) => (
-    <span id="calendar">
-        <div id="calendarContain">
+    <table id="calendar">
+        <tbody id="calendarContain">
         {events.map(event => {
             const date = new Date(event.start.dateTime || event.start.date);
             const time = date.getHours() ? ' ' + fixPrefix(date.getHours()) + ':' + fixPrefix(date.getMinutes()) : '';
             const dateString = fixPrefix(date.getDate()) + '.' + fixPrefix(date.getMonth() + 1) + time;
-            return <div key={shortid.generate()}>{dateString + ' - ' + event.summary}</div>;
+            return (
+                <tr key={shortid.generate()}>
+                    <td className="calendarDate">{dateString}</td>
+                    <td className="calendarSummary">{event.summary}</td>
+                </tr>
+            );
         })}
-        </div>
-    </span>
+        </tbody>
+    </table>
 );
 
 export default CalendarView;
