58d5f5199ba3e957d5c22a4fc383470ad44cdf96
Jonas Jensen <jonasj@mesan.no>
2016-09-04 11:06:39 +0200

    change app to keep-alive by refreshing every 29 minutes..

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import Calendar from "./Calendar";
 import Shoutbox from "./Shoutbox";
 import XkcdStrip from "./XkcdStrip";
 
-const App = () => (
-    <div className="App">
-        <Calendar/>
-        <Shoutbox/>
-        <XkcdStrip/>
-    </div>
-);
+class App extends Component {
+    constructor() {
+        super();
+        this.HARD_REFRESH_EVERY = 1000 * 60 * 29;
+    }
+
+    componentDidMount() {
+        setTimeout(() => location.reload(), this.HARD_REFRESH_EVERY)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Calendar/>
+                <Shoutbox/>
+                <XkcdStrip/>
+            </div>
+        );
+    }
+}
 export default App;
